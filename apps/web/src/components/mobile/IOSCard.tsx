7e904b7c173f83a34b6da63bad8e403adf5e5a43
Z User <z@container>
2026-05-20 11:25:46 +0000

    c325647d-2a0e-4f0b-8bb4-7df69103be1b

--- apps/web/src/components/mobile/IOSCard.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-export default function IOSCard({ children, onClick, highlight = false, noMargin = false }: { children: React.ReactNode; onClick?: () => void; highlight?: boolean; noMargin?: boolean }) {
-  return (
-    <div className={`m-card ${highlight ? 'm-card--hl' : ''}`} onClick={onClick} style={{ cursor: onClick ? 'pointer' : 'default', margin: noMargin ? 0 : undefined }}>
-      {children}
-    </div>
-  )
-}
